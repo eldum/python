bd277c4001a684339545bc5959060996a6ef5fb3
Rubén Adi Duma <rubenadiduma@gmail.com>
2025-09-20 15:05:28 +0200

    Add files via upload

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,10 +1,8 @@
+lista = []
 
-numero1 = int(input("introduce el 1º:"))
-numero2 = int(input("introduce el 2º:"))
-numero3 = int(input("introduce el 3º:"))
-numero4 = int(input("introduce el 4º:"))
+for i in range(4):
+    lista.append(int(input("número:")))
 
-lista = [numero1, numero2, numero3, numero4]
 media = sum(lista) / len(lista)
 
-print(f"la media de {numero1}, {numero2}, {numero3}, {numero4} es {media}")
+print(media)
